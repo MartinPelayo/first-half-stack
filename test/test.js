5e6c2331e9eb4fd0c501159afc1c7a11d8dcd0e1
Martin <martinconradpelayo@gmail.com>
2017-04-26 09:01:39 -0700

    started second test

---
+++ test/test.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+const assert = chai.assert;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const app = require('../lib/app');
+const connection = require('../lib/connect');
+
+const request = chai.request(app);
+
+describe('Get / ', () => {
+  it('says... WHAT UP WORLD!', () => {
+    return request
+      .get('/')
+      .then(res => res.text)
+      .then(text => assert.equal(text, 'What up world!'));
+  });
+
+  function savedDoggo(dog) {
+    return request.post('/dogs')
+    .send(dog);
+  }
+
+  const dawg = {
+    name: 'dawg',
+    type: 'dog'
+  };
+
+  it('saves a doggo', () => {
+    return savedDoggo(dawg)
+    .then(res => res.body)
+    .then(savedDawg => {
+      assert.isOk(savedDawg._id);
+      dawg._id = savedDawg._id;
+      assert.deepEqual(savedDawg, dawg);
+    });
+  });
+});
