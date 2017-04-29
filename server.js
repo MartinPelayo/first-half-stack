1092da6b08ff75b39ecff788c0782d7a1e3486e1
Martin <martinconradpelayo@gmail.com>
2017-04-29 11:16:13 -0700

    trying to get rid of a stupid server error

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const app = require('./lib/app');
 const http = require('http');
 const DB_URI = 'mongodb://localhost:27017/doggos';
 
-// const connection = require('./lib/connect');
-// connection.connect(DB_URI);
+const connection = require('./lib/connect');
+connection.connect(DB_URI);
 
 const server = http.createServer(app);
 
--- test/test.js
+++ test/test.js
@@ -16,23 +16,55 @@ describe('Get / ', () => {
       .then(text => assert.equal(text, 'What up world!'));
   });
 
-  function savedDoggo(dog) {
-    return request.post('/dogs')
-    .send(dog);
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // function savedDoggo(dog) {
+  //   return request.post('/dogs')
+  //   .send(dog);
+  // }
 
   const dawg = {
     name: 'dawg',
     type: 'dog'
   };
 
-  it('saves a doggo', () => {
-    return savedDoggo(dawg)
+  // it('saves a doggo', () => {
+  //   return savedDoggo(dawg)
+  //   .then(res => res.body)
+  //   .then(savedDawg => {
+  //     assert.isOk(savedDawg._id);
+  //     dawg._id = savedDawg._id;
+  //     assert.deepEqual(savedDawg, dawg);
+  //   });
+  // });
+
+
+
+
+it ('GETs doggo is if exists', () => {
+  return request
+    .get(`/dogs/${dawg._id}`)
     .then(res => res.body)
-    .then(savedDawg => {
-      assert.isOk(savedDawg._id);
-      dawg._id = savedDawg._id;
-      assert.deepEqual(savedDawg, dawg);
-    });
-  });
+    .then(dog => assert.deepEqual(dog, dawg));
+});
+
+
+
+
+
 });
