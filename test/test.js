5e499c3226789586b2f7963377d3955cb7ce25a7
Martin <martinconradpelayo@gmail.com>
2017-04-30 18:30:21 -0700

    server working and tests completed and pass

--- test/test.js
+++ test/test.js
@@ -18,47 +18,56 @@ describe('Get / ', () => {
 });
 
 
+describe('doggos REST API!', () => {
+  const DB_URI = 'mongodb://localhost:27017/dogs';
 
+  before(() => connection.connect(DB_URI));
+  before(() => connection.db.dropDatabase());
+  after(() => connection.close());
 
+  function saveDog(dog) {
+    return request
+      .post('/dogs')
+      .send(dog);
+  }
 
+  const dawg = {
+    name: 'dawg',
+    type: 'dog'
+  };
+  
+  it('saves a doggo', () => {
+    return saveDog(dawg)
+    .then(res => res.body)
+    .then(savedDawg => {
+      assert.isOk(savedDawg._id);
+      dawg._id = savedDawg._id;
+      assert.deepEqual(savedDawg, dawg);
+    });
+  });
 
+  it ('GETs doggo is if it exists', () => {
+    return request
+      .get(`/dogs/${dawg._id}`)
+      .then(res => res.body)
+      .then(dog => assert.deepEqual(dog, dawg));
+  });
 
+  it('We get 404 if there is no doggo', () => {
+    return request
+    .get('/dogs/doesnotexist')
+    .then(
+      () => {
+        throw new Error('successful status code not expected');
+      },
+      res => {
+        assert.equal(res.status, 404);
+        assert.isOk(res.response.body.error);
+      }
+    );
+  });
 
-
-
-
-
-
-  // function savedDoggo(dog) {
-  //   return request.post('/dogs')
-  //   .send(dog);
-  // }
-
-  // const dawg = {
-  //   name: 'dawg',
-  //   type: 'dog'
-  // };
-
-  // it('saves a doggo', () => {
-  //   return savedDoggo(dawg)
-  //   .then(res => res.body)
-  //   .then(savedDawg => {
-  //     assert.isOk(savedDawg._id);
-  //     dawg._id = savedDawg._id;
-  //     assert.deepEqual(savedDawg, dawg);
-  //   });
-  // });
-
-
-
-
-// it ('GETs doggo is if it exists', () => {
-//   return request
-//     .get(`/dogs/${dawg._id}`)
-//     .then(res => res.body)
-//     .then(dog => assert.deepEqual(dog, dawg));
-// });
-
+});
 
 
 
